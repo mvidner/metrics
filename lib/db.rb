22671a4d16a3c2ca66bac86b17d0513b6982bccc
Martin Vidner <martin@vidner.net>
2014-12-05 17:21:46 +0100

    Enable concurrent database access (5s timeout).

--- lib/db.rb
+++ lib/db.rb
@@ -2,4 +2,8 @@ require_relative "model"
 
 filename = "metrics.sqlite3"
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: filename)
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: filename,
+  timeout:  5000                # ms
+)
